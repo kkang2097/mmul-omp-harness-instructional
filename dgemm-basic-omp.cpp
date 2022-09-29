72e4768e2cdc226c23fa69e2958977de8e69d4d0
Elliot Kang <kkang2097@gmail.com>
2022-09-29 16:23:22 -0700

    basic code

--- dgemm-basic-omp.cpp
+++ dgemm-basic-omp.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <omp.h>
 #include "likwid-stuff.h"
+#include <vector>
+#include <algorithm>
+#include <random>
+#include <omp.h>
+#include <unistd.h>
 
 const char* dgemm_desc = "Basic implementation, OpenMP-enabled, three-loop dgemm.";
 
@@ -10,13 +15,35 @@ const char* dgemm_desc = "Basic implementation, OpenMP-enabled, three-loop dgemm
  * where A, B, and C are n-by-n matrices stored in column-major format.
  * On exit, A and B maintain their input values.
  */
-void square_dgemm(int n, double* A, double* B, double* C) 
+void square_dgemm(int n, double* A, double* B, double* C)
 {
    // insert your code here: implementation of basic matrix multiply with OpenMP parallelism enabled
 
    // be sure to include LIKWID_MARKER_START(MY_MARKER_REGION_NAME) inside the block of parallel code,
    // but before your matrix multiply code, and then include LIKWID_MARKER_STOP(MY_MARKER_REGION_NAME)
    // after the matrix multiply code but before the end of the parallel code block.
+   int sleep_duration_sec = 2;
 
    std::cout << "Insert your basic matrix multiply, openmp-parallel edition here " << std::endl;
+
+   #pragma omp parallel
+    {
+
+      #pragma omp parallel for
+      //Then add our triple for-loop
+       for (int i = 0; i < n; i++){
+         for(int j = 0; j < n; j++){
+           //C[i*n + j] = 0;
+           for(int k = 0; k < n; k++){
+             //
+            //Row major order:
+            //prod[i][j] += A[i][k] + B[k][j]
+            //Column major order:
+            //prod[i][j] += A[k][j] + B[i][k]
+            C[i*n + j] = C[i*n + j] + A[k*n + j] * B[i*n + k];
+           }
+         }
+       }
+    }
+
 }
--- dgemm-blocked-omp.cpp
+++ dgemm-blocked-omp.cpp
@@ -11,7 +11,7 @@ const char* dgemm_desc = "Blocked dgemm, OpenMP-enabled";
  *  C := C + A * B
  * where A, B, and C are n-by-n matrices stored in column-major format.
  * On exit, A and B maintain their input values. */
-void square_dgemm_blocked(int n, int block_size, double* A, double* B, double* C) 
+void square_dgemm_blocked(int n, int block_size, double* A, double* B, double* C)
 {
    // insert your code here: implementation of blocked matrix multiply with copy optimization and OpenMP parallelism enabled
 
@@ -20,4 +20,30 @@ void square_dgemm_blocked(int n, int block_size, double* A, double* B, double* C
    // after the matrix multiply code but before the end of the parallel code block.
 
    std::cout << "Insert your blocked matrix multiply with copy optimization, openmp-parallel edition here " << std::endl;
+
+   #pragma omp parallel
+    {
+
+      #pragma omp parallel for
+         //Then add our triple for-loop
+         for (int ii = 0; ii < n; ii+= block_size){
+           for(int jj = 0; jj < n; jj+= block_size){
+             //C[i*n + j] = 0;
+             for(int kk = 0; kk < n; kk+= block_size){
+               //
+              //Row major order:
+              //prod[i][j] += A[i][k] + B[k][j]
+              //Column major order:
+              //prod[i][j] += A[k][j] + B[i][k]
+                //Block multiply
+                //info src: https://netlib.org/utk/papers/autoblock/node2.html
+                for (int i = ii; i < ii+block_size; i++){
+                  for(int j = jj; j < jj+block_size; j++){
+                    //C[i*n + j] = 0;
+                    for(int k = kk; k < kk+block_size; k++){
+                      C[i*n + j] = C[i*n + j] + A[k*n + j] * B[i*n + k];
+                    }}}
+             }}}
+
+    }
 }
